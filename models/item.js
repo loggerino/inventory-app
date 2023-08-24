bca5a20fde99dc04335c6483a2c572ea05810693
Chyisiang <chyisiang8@gmail.com>
2023-08-24 20:09:31 +0800

    models

---
+++ models/item.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    stock: { type: Number, required: true },
+    size: [{ type: String }],
+    category: { type: Schema.Types.ObjectId, ref: "category" },
+});
+
+ItemSchema.virtual("url").get(function () {
+    return `/item/${this._id}`;
+});
+
+module.exports = mongoose.model("Item", ItemSchema);
