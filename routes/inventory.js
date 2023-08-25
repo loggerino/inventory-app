964bd19506753614b68d2059c2aa417edefd478f
Chyisiang <chyisiang8@gmail.com>
2023-08-25 20:32:59 +0800

    images

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 const categoryController = require('../controllers/categoryController');
 const brandController = require('../controllers/brandController');
 const itemController = require('../controllers/itemController');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/images');
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + '-' + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage });
 
 // item routes
 router.get('/item/create', itemController.itemCreateGet);
-router.post('/item/create', itemController.itemCreatePost);
+router.post('/item/create', upload.single('image'), itemController.itemCreatePost);
 router.get('/item/:id/update', itemController.itemUpdateGet);
-router.post('/item/:id/update', itemController.itemUpdatePost);
+router.post('/item/:id/update', upload.single('image'), itemController.itemUpdatePost);
 router.get('/item/:id/delete', itemController.itemDeleteGet);
 router.post('/item/:id/delete', itemController.itemDeletePost);
 router.get('/item/:id', itemController.itemDetail);
